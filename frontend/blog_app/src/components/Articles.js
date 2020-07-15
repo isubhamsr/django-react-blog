a03abeb168c65198d63300a72bbd0d6f8010b13e
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2020-07-15 14:51:58 +0530

    add boostrap on layout

--- frontend/blog_app/src/components/Articles.js
+++ frontend/blog_app/src/components/Articles.js
@@ -1,62 +1,108 @@
-import React from 'react'
-import { List, Avatar, Space } from 'antd';
-import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
+// import React from 'react'
+// import { List, Avatar, Space } from 'antd';
+// import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
+// import { Link } from 'react-router-dom'
+
+// const IconText = ({ icon, text }) => (
+//     <Space>
+//         {React.createElement(icon)}
+//         {text}
+//     </Space>
+// );
+
+// export default function Articles(props) {
+//     console.log("form articles");
+//     console.log(props.data);
+
+//     return (
+//         <List
+//             itemLayout="vertical"
+//             size="large"
+//             pagination={{
+//                 onChange: page => {
+//                     console.log(page);
+//                 },
+//                 pageSize: 5,
+//             }}
+//             dataSource={props.data}
+//             //         footer={
+//             //             <div>
+//             //                 <b>ant design</b> footer part
+//             //   </div>
+//             //         }
+//             renderItem={item => (
+
+//                 <List.Item
+//                     key={item.article_title}
+//                     actions={[
+//                         <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+//                         <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+//                         <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+//                     ]}
+//                     extra={
+
+//                         <img
+//                             width={250}
+//                             height={150}
+//                             alt="logo"
+//                             src={"http://127.0.0.1:8000/media/" + item.article_image}
+//                         />
+//                     }
+//                 >
+//                     <List.Item.Meta
+//                         avatar={<Avatar src={item.avatar} />}
+//                         title={<Link to={`/post/${item.article_id}`}>{item.article_title}</Link>}
+//                         description={item.article_description.slice(0, 50)}
+//                     />
+//                     {`${item.article_description.slice(0, 670)}...`}
+//                 </List.Item>
+//             )}
+//         />
+//     )
+// }
+
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-const IconText = ({ icon, text }) => (
-    <Space>
-      {React.createElement(icon)}
-      {text}
-    </Space>
-  );
+export default class Articles extends Component {
 
-export default function Articles(props) {
-    console.log("form articles");
-    console.log(props.data);
-    
-    return (
-        <List
-            itemLayout="vertical"
-            size="large"
-            pagination={{
-                onChange: page => {
-                    console.log(page);
-                },
-                pageSize: 5,
-            }}
-            dataSource={props.data}
-    //         footer={
-    //             <div>
-    //                 <b>ant design</b> footer part
-    //   </div>
-    //         }
-            renderItem={item => (
-                
-                <List.Item
-                    key={item.article_title}
-                    actions={[
-                        <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-                        <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-                        <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
-                    ]}
-                    extra={
-                        
-                        <img
-                            width={250}
-                            height={150}
-                            alt="logo"
-                            src= {"http://127.0.0.1:8000/media/"+item.article_image}
-                        />
-                    }
-                >
-                    <List.Item.Meta
-                        avatar={<Avatar src={item.avatar} />}
-                        title={<Link to={`/post/${item.article_id}`}>{item.article_title}</Link>}
-                        description={item.article_description.slice(0,50)}
-                    />
-                    {`${item.article_description.slice(0,670)}...`}
-                </List.Item>
-            )}
-        />
-    )
+    render() {
+        return (
+          <>
+          {this.props.data.map((item)=>(
+            <>
+            <div class="post-preview my-3">
+          <Link to={`/post/${item.article_id}`}>
+          <img src={"http://127.0.0.1:8000/media/" + item.article_image} class="rounded float-left mx-3" alt="ok" width={250} height={130} />
+            <h2 class="post-title">
+            {item.article_title}
+            </h2>
+            <p class="post-subtitle">
+              {`${item.article_description.slice(0, 380)}...`}
+            </p>
+          </Link>
+          <p class="post-meta">Posted by
+            <a href="#" target="_blank"> Start Bootstrap </a>
+            on September 24, 2019</p>
+        </div>
+        <hr/>
+        </>
+          ))}
+            </>
+          
+        )
+    }
 }
+
+
+// {this.props.data.map((item)=>(
+//   <ul class="list-unstyled">
+//     <li class="media">
+//         <img class="mr-3"  src={"http://127.0.0.1:8000/media/" + item.article_image} alt="Generic placeholder image" width="250px" height="150px"/>
+//             <div class="media-body">
+//                 <h3 class="mt-0 mb-1">{item.article_title}</h3>
+// <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+// </div>
+// </li>
+// </ul>
+// ))}
--- frontend/blog_app/src/containers/Layout.js
+++ frontend/blog_app/src/containers/Layout.js
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react'
+import React, { Component } from 'react'
 import { Layout, Menu, Breadcrumb } from 'antd';
 import { Link, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
@@ -13,57 +13,114 @@ class CustomLayout extends Component {
         redirect: false
     }
 
-    handleLogout = () =>{
+    handleLogout = () => {
         this.props.onLogout()
         this.setState({ redirect: true })
-        return( <Redirect to="/signin" />)
+        return (<Redirect to="/signin" />)
     }
 
     render() {
         return (
-            <Layout className="layout">
-            <Header>
-                <div className="logo" />
-                <Menu theme="dark" mode="horizontal" >
-                {/* defaultSelectedKeys={['1']} */}
-                    <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
-                    {
-                        this.props.idAuthenticated ?
-                        <Menu.Item key="2" onClick={this.handleLogout}>Log Out</Menu.Item>
-                        :
-                        <Menu.Item key="2"><Link to="/signin">Sign In</Link></Menu.Item>
-                    }
-                </Menu>
-            </Header>
-            <Content style={{ padding: '0 50px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                    <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
-                    <Breadcrumb.Item><Link to="/">List</Link></Breadcrumb.Item>
-                    {/* <Breadcrumb.Item>App</Breadcrumb.Item> */}
-                </Breadcrumb>
-                <div className="site-layout-content">
-                    {this.props.children}
-                </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-        </Layout>
+            <body class="d-flex flex-column h-100">
+
+                <header>
+                    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                        <div class="container-fluid">
+                            <Link class="navbar-brand" to="/">Coding School</Link>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarCollapse">
+                                <ul class="navbar-nav mr-auto mb-2 mb-md-0">
+                                    <li class="nav-item active">
+                                        <Link class="nav-link" aria-current="page" to="#">Home</Link>
+                                    </li>
+                                    <li class="nav-item">
+                                        <Link class="nav-link" to="#">Link</Link>
+                                    </li>
+                                    <li class="nav-item">
+                                        <Link class="nav-link disabled" to="#" tabindex="-1" aria-disabled="true">Disabled</Link>
+                                    </li>
+                                </ul>
+                                <form class="d-flex">
+                                    <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search" />
+                                    <button class="btn btn-outline-success" type="submit">Search</button>
+                                </form>
+                                {
+                                    this.props.idAuthenticated ?
+                                    <button class="btn btn-outline-success m-2" onClick={this.handleLogout}>Log Out</button>
+                                    :
+                                    <Link to="/signin" class="btn btn-outline-success m-2" onClick={this.handleLogout}>Sign In</Link>
+                                }
+                            </div>
+                        </div>
+                    </nav>
+                </header>
+
+                <main class="flex-shrink-0">
+                    <div class="container">
+                        {this.props.children}
+                    </div>
+                </main>
+
+                <footer class="footer mt-auto py-3 bg-light">
+                    <div class="container">
+                        <span class="text-muted">Place sticky footer content here.</span>
+                    </div>
+                </footer>
+            </body>
+
         )
     }
 }
 
 
-const mapStateToProps = (state)=>{
-    return{
+const mapStateToProps = (state) => {
+    return {
         loading: state.loading,
-        err : state.err
+        err: state.err
     }
 }
 
-const mapDispatchToProps = (dispatch) =>{
-    return{
-        onLogout : ()=>{dispatch(actions.authLogout())}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onLogout: () => { dispatch(actions.authLogout()) }
     }
 }
 
 
-export default connect(mapStateToProps,mapDispatchToProps)(CustomLayout)
+
+
+
+
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CustomLayout)
+
+// <Layout className="layout">
+//             <Header>
+//                 <div className="logo" />
+//                 <Menu theme="dark" mode="horizontal" >
+//                 {/* defaultSelectedKeys={['1']} */}
+//                     <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
+//                     {
+//                         this.props.idAuthenticated ?
+//                         <Menu.Item key="2" onClick={this.handleLogout}>Log Out</Menu.Item>
+//                         :
+//                         <Menu.Item key="2"><Link to="/signin">Sign In</Link></Menu.Item>
+//                     }
+//                 </Menu>
+//             </Header>
+//             <Content style={{ padding: '0 50px' }}>
+//                 <Breadcrumb style={{ margin: '16px 0' }}>
+//                     <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
+//                     <Breadcrumb.Item><Link to="/">List</Link></Breadcrumb.Item>
+//                     {/* <Breadcrumb.Item>App</Breadcrumb.Item> */}
+//                 </Breadcrumb>
+//                 <div className="site-layout-content">
+//                     {this.props.children}
+//                 </div>
+//             </Content>
+//             <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+//         </Layout>
