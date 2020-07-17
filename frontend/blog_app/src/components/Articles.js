33fd00c15f120e049cf02c05ce81d8120f34fc07
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2020-07-17 21:20:46 +0530

    add load more

--- frontend/blog_app/src/components/Articles.js
+++ frontend/blog_app/src/components/Articles.js
@@ -1,76 +1,22 @@
-// import React from 'react'
-// import { List, Avatar, Space } from 'antd';
-// import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
-// import { Link } from 'react-router-dom'
-
-// const IconText = ({ icon, text }) => (
-//     <Space>
-//         {React.createElement(icon)}
-//         {text}
-//     </Space>
-// );
-
-// export default function Articles(props) {
-//     console.log("form articles");
-//     console.log(props.data);
-
-//     return (
-//         <List
-//             itemLayout="vertical"
-//             size="large"
-//             pagination={{
-//                 onChange: page => {
-//                     console.log(page);
-//                 },
-//                 pageSize: 5,
-//             }}
-//             dataSource={props.data}
-//             //         footer={
-//             //             <div>
-//             //                 <b>ant design</b> footer part
-//             //   </div>
-//             //         }
-//             renderItem={item => (
-
-//                 <List.Item
-//                     key={item.article_title}
-//                     actions={[
-//                         <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-//                         <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-//                         <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
-//                     ]}
-//                     extra={
-
-//                         <img
-//                             width={250}
-//                             height={150}
-//                             alt="logo"
-//                             src={"http://127.0.0.1:8000/media/" + item.article_image}
-//                         />
-//                     }
-//                 >
-//                     <List.Item.Meta
-//                         avatar={<Avatar src={item.avatar} />}
-//                         title={<Link to={`/post/${item.article_id}`}>{item.article_title}</Link>}
-//                         description={item.article_description.slice(0, 50)}
-//                     />
-//                     {`${item.article_description.slice(0, 670)}...`}
-//                 </List.Item>
-//             )}
-//         />
-//     )
-// }
-
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 export default class Articles extends Component {
+
+  state = {
+    visible: 5,
+  }
+
+  loadMore = () =>{
+    this.setState((prev) => {
+      return {visible: prev.visible + 5};
+    });
+  }
 
     render() {
         return (
           <div class="post-preview my-4">
-          {this.props.data.map((item)=>(
+          {this.props.data.slice(0, this.state.visible).map((item)=>(
             <>
             {/* <div class="post-preview my-4"> */}
           {/* <Link to={`/post/${item.article_id}`}> */}
@@ -91,21 +37,31 @@ export default class Articles extends Component {
         <hr class="my-4"/>
         </>
           ))}
+          {this.state.visible < this.props.data.length &&
+ <button onClick={this.loadMore} type="button" className="load-more">Load more</button>
+}
             </div>
           
         )
     }
 }
 
-
-// {this.props.data.map((item)=>(
-//   <ul class="list-unstyled">
-//     <li class="media">
-//         <img class="mr-3"  src={"http://127.0.0.1:8000/media/" + item.article_image} alt="Generic placeholder image" width="250px" height="150px"/>
-//             <div class="media-body">
-//                 <h3 class="mt-0 mb-1">{item.article_title}</h3>
-// <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+// {this.state.items.slice(0, this.state.visible).map((item, index) => {
+//   return (
+//     <div className="tile fade-in" key={item.id}>
+//       <span className="count">{index+1}</span>
+//       <h2>{item.title}</h2>
+//       <p>{item.body}</p>
+//     </div>
+//   );
+// })}
 // </div>
-// </li>
-// </ul>
-// ))}
+// {this.state.visible < this.state.items.length &&
+//  <button onClick={this.loadMore} type="button" className="load-more">Load more</button>
+// }
+
+// loadMore() {
+//   this.setState((prev) => {
+//     return {visible: prev.visible + 4};
+//   });
+// }
